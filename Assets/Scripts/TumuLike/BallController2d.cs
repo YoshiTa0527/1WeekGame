1eb3557f4fd8855ea31a66d8f51ff8e69c397976
RyutoShimada <business.sub10110@gmail.com>
2021-04-13 18:05:05 +0900

    ２Dで作った

---
+++ Assets/Scripts/TumuLike/BallController2d.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController2d : MonoBehaviour
+{
+    [SerializeField] float m_pushPower = 1f;
+    Rigidbody2D m_rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float random = Random.Range(-1f, 1f);
+        m_rb = GetComponent<Rigidbody2D>();
+        m_rb.AddForce(new Vector2(random, 0) * m_pushPower, ForceMode2D.Impulse);
+    }
+}
---
+++ Assets/Scripts/TumuLike/Chaincontroller.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chaincontroller : MonoBehaviour
+{
+    RaycastHit hit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos = Input.mousePosition;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(mousePos);
+            
+            if (Physics.Raycast(ray,out hit, 100f))
+            {
+                if (hit.collider.tag == "Ball")
+                {
+                    //Destroy(hit.collider.gameObject);
+                    //hit.collider.gameObject.GetComponent<Renderer>().material.SetColor("_EmissionColor", new Color(1f, 1f, 0, 0.5f));
+
+                }
+            }
+        }
+    }
+}
